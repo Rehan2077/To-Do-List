0b2f23670b412fc1e2c5ee091f84fd00ed4b5c04
Rehan2077 <rfazal0124@gmail.com>
2023-07-21 18:45:52 +0530

    Added responsiveness

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const inputBox = document.getElementById('input-box');
 const listContainer = document.getElementById('list-container');
 
 function addTask() {
-    if(inputBox.value == ''){
+    if (inputBox.value == '') {
         alert("You must write something!")
     }
-    else{
+    else {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
@@ -18,21 +18,27 @@ function addTask() {
 }
 
 listContainer.addEventListener('click', (e) => {
-    if(e.target.tagName ==="LI"){
+    if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked")
         saveData();
     }
-    else if(e.target.tagName ==="SPAN"){
+    else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
         saveData();
     }
 })
 
+inputBox.addEventListener('keydown', (e) => {
+    // e.preventDefault()
+    if (e.keyCode === 13) {
+        addTask();
+    }
+})
 function saveData() {
-    localStorage.setItem("data", listContainer.innerHTML);
-}
-function showData(){
-    listContainer.innerHTML = localStorage.getItem("data")
-}
+        localStorage.setItem("data", listContainer.innerHTML);
+    }
+function showData() {
+        listContainer.innerHTML = localStorage.getItem("data")
+    }
 
 showData()
